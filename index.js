e9dd3a2fda71094d958e17cabd3f34a6dcec0826
MaithiliBhurke <maithilibhurke10.model@gmail.com>
2024-06-30 20:34:59 +0530

    new project

---
+++ index.js
@@ -0,0 +1,54 @@
+import cart from "./cart.js";
+import products from "./products.js";
+
+let app = document.getElementById('app');
+let temporaryContent = document.getElementById('temporaryContent');
+
+//load template file
+const loadTemplate = () => {
+    fetch('/template.html')
+        .then(response => response.text())
+        .then(html => {
+            app.innerHTML = html;
+            let contentTab = document.getElementById('contentTab');
+            contentTab.innerHTML = temporaryContent.innerHTML;
+            temporaryContent.innerHTML = null;
+            cart();
+            initApp();
+        })
+}
+loadTemplate();
+
+const initApp = () => {
+    //load list of products
+    // console.log(products);
+    let listProductHTML = document.querySelector('.listProduct');
+    listProductHTML.innerHTML = null;
+
+    products.forEach(product => {
+        let newProduct = document.createElement('div');
+        newProduct.classList.add('item');
+        newProduct.innerHTML =
+            `
+            <a href="/detail.html?id=${product.id}">
+                <img src="${product.image}"/>
+            </a>
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>
+            <button class="addCart" data-id='${product.id}'>
+                Add To Cart
+            </button>`;
+
+        {/* <a href="/detail.html?id=${product.id}"></a> */ }
+        listProductHTML.appendChild(newProduct);
+    });
+
+}
+
+
+
+
+
+
+
+
---
+++ products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        "id": 1,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/1.png"
+    },
+    {
+        "id": 2,
+        "name": "LD02 LOUNGHE CHAIR",
+        "price": 300,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/2.png"
+    },
+    {
+        "id": 3,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/3.png"
+    },
+    {
+        "id": 4,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 250,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/4.png"
+    },
+    {
+        "id": 5,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/5.png"
+    },
+    {
+        "id": 6,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/6.png"
+    },
+    {
+        "id": 7,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/7.png"
+    },
+    {
+        "id": 8,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 350,
+        "description": "Expertly rendered by Carl Hansen & Son, the lounge chair-first introduced in 1951 and enduring ever since-is available",
+        "image": "imgs/8.png"
+    }
+];
+export default products;
+
+
+
+
+
+
+
+
+
